1ef8dccd3777b0ebd340d27cc21462de8dd61521
SEOYOUNG-cloud <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2021-06-20 19:20:39 +0900

    blockchain_1
    hyperledger fabric_display on the web

---
+++ block-qr/stream-music/application/sdk/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+var path = require('path');
+var sdk = require('./sdk');
+
+const PORT = 8080;
+const HOST = 'localhost';
+
+app.get('/api/getWallet', function (req, res) {
+    var walletid = req.query.walletid;
+
+    let args = [walletid];
+
+    sdk.send(false, 'getWallet', args, res);
+});
+app.get('/api/setMusic', function (req, res) {
+    var serialnum = req.query.serialnum;
+
+    let args = [serialnum];
+    sdk.send(true, 'setMusic', args, res);
+});
+app.get('/api/getAllmusic', function (req, res) {
+    var serialnum = req.query.serialnum;
+
+    let args = [serialnum];
+    sdk.send(false, 'getAllMusic', args, res);
+});
+/*
+app.get('/api/purchaseMusic', function (req, res) {
+    var walletid = req.query.walletid;
+    var key = req.query.musickey;
+    
+    let args = [walletid, key];
+    sdk.send(true, 'purchaseMusic', args, res);
+});
+*/
+app.use(express.static(path.join(__dirname, './client')));
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
