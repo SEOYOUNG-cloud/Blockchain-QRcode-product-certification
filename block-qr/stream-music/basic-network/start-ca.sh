a1f5088982495fa00d7c3dbd33466926556f8efa
heeyeon9 <78019287+heeyeon9@users.noreply.github.com>
2021-06-23 01:24:03 +0900

    Update start-ca.sh

--- block-qr/stream-music/basic-network/start-ca.sh
+++ block-qr/stream-music/basic-network/start-ca.sh
@@ -4,7 +4,7 @@ set -ev
 docker-compose -f docker-compose-ca.yaml up -d ca.sales1.acornpub.com
 
 sleep 1
-cd $GOPATH/src/stream-music/application/sdk
+cd block-qr/stream-music/application/sdk
 node enrollAdmin.js
 sleep 1
 node registUsers.js
