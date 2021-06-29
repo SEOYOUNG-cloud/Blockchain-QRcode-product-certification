1294fe628dff6d95dd59db30e1f87f3beab2db1b
heeyeon9 <78019287+heeyeon9@users.noreply.github.com>
2021-06-29 19:00:34 +0900

    Update install-cc.sh

--- block-qr/stream-music/basic-network/install-cc.sh
+++ block-qr/stream-music/basic-network/install-cc.sh
@@ -5,11 +5,11 @@ set -ev
 docker exec cli peer chaincode install -n music-cc -v 1.0 -p chaincode/go
 sleep 1
 # instantiate chaincode for channelsales1
-docker exec cli peer chaincode instantiate -o orderer.acornpub.com:7050 -C channelsales1 -n music-cc -v 1.0 -c '{"Args":[""]}' -P "OR ('Sales1Org.member','CustomerOrg.member')"
+docker exec cli peer chaincode instantiate -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -v 1.0 -c '{"Args":[""]}' -P "OR ('Sales1Org.member','CustomerOrg.member')"
 sleep 10
 # invoke chaincode for channelsales1
-docker exec cli peer chaincode invoke -o orderer.acornpub.com:7050 -C channelsales1 -n music-cc -c '{"function":"initWallet","Args":[""]}'
-docker exec cli peer chaincode invoke -o orderer.acornpub.com:7050 -C channelsales1 -n music-cc -c '{"function":"setMusic","Args":["1Q2W3E4R"]}'
+docker exec cli peer chaincode invoke -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"initWallet","Args":[""]}'
+docker exec cli peer chaincode invoke -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"setProduct","Args":["1Q2W3E4R","AP0214","Chanel"]}'
 sleep 3
 # query chaincode for channelsales1
-docker exec cli peer chaincode query -o orderer.acornpub.com:7050 -C channelsales1 -n music-cc -c '{"function":"getAllMusic","Args":["1Q2W3E4R"]}'
+docker exec cli peer chaincode query -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"getAllProduct","Args":["1Q2W3E4R"]}'
