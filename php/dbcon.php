7aaf6e3b9fc5abd110a80b8c1aee6673c9e4e6fa
SEOYOUNG-cloud <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2021-07-26 19:37:09 +0900

    php
    php file

--- php/dbcon.php
+++ php/dbcon.php
@@ -1,42 +1,46 @@
 <?php
 
-    $host = 'localhost';
-    $username = 'user1'; # MySQL 계정 아이디
-    $password = 'gdgd11'; # MySQL 계정 패스워드
-    $dbname = 'unum';  # DATABASE 이름
+   $host = 'localhost';
+   $username = 'manager';
+   $password = 'root';
+   $dbname = 'member';
+
+   
+   $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+
+   try{
+
+       $con = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8",$username, $password);
+   } catch(PDOexception $e){
+
+       die("Failed to connect to the database: " . $e->getMessage());
+   }
 
 
-    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-    
-    try {
+   $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   $con->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-        $con = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8",$username, $password);
-    } catch(PDOException $e) {
+   if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()){
 
-        die("Failed to connect to the database: " . $e->getMessage()); 
-    }
+       function undo_magic_quotes_gpc(&$array){
+           foreach($array as &$value) {
+               if(is_array($value)){
+                   undo_magic_quotes_gpc($value);
+               }
+               else{
+                   $value = stripslashes($value);
+               }
+           }
+       }
 
 
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $con->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+       undo_magic_quotes_gpc($_POST);
+       undo_magic_quotes_gpc($_GET);
+       undo_magic_quotes_gpc($_COOKIE);
 
-    if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) { 
-        function undo_magic_quotes_gpc(&$array) { 
-            foreach($array as &$value) { 
-                if(is_array($value)) { 
-                    undo_magic_quotes_gpc($value); 
-                } 
-                else { 
-                    $value = stripslashes($value); 
-                } 
-            } 
-        } 
- 
-        undo_magic_quotes_gpc($_POST); 
-        undo_magic_quotes_gpc($_GET); 
-        undo_magic_quotes_gpc($_COOKIE); 
-    } 
- 
-    header('Content-Type: text/html; charset=utf-8'); 
-    #session_start();
+   }
+
+   header('Content-Type: text/html; charset=utf-8');
+
+
 ?>
