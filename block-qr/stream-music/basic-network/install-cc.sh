3a7526ce3d9a8bb0a67c18d97d6fc879f9f1a3a0
heeyeon9 <78019287+heeyeon9@users.noreply.github.com>
2021-08-02 02:39:25 +0900

    Update install-cc.sh

--- block-qr/stream-music/basic-network/install-cc.sh
+++ block-qr/stream-music/basic-network/install-cc.sh
@@ -7,8 +7,25 @@ sleep 1
 # instantiate chaincode for channelsales1
 docker exec cli peer chaincode instantiate -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -v 1.0 -c '{"Args":[""]}' -P "OR ('Sales1Org.member','CustomerOrg.member')"
 sleep 10
+peer chaincode invoke -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"initSupply","Args":[""]}'
+sleep 3
+docker exec cli peer chaincode query -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"getSupply","Args":["ABCDEFGH"]}'
+docker exec cli peer chaincode query -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"getSupply","Args":["1Q2W3E4R"]}'
+docker exec cli peer chaincode query -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"getSupply","Args":["5T6Y7U8I"]}'
+docker exec cli peer chaincode query -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"getSupply","Args":["A1S2D3F4"]}'
 # invoke chaincode for channelsales1
-docker exec cli peer chaincode invoke -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"setProduct","Args":["1Q2W3E4R","AP0214","Chanel"]}'
+docker exec cli peer chaincode invoke -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"setProduct","Args":["G5H6J7K8","AP0250","Chanel"]}'
+docker exec cli peer chaincode query -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"getSupply","Args":["G5H6J7K8"]}'
+sleep 3
+docker exec cli peer chaincode invoke -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"setSupply","Args":["G5H6J7K8","Arrived-D"]}'
+docker exec cli peer chaincode query -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"getSupply","Args":["G5H6J7K8"]}'
+
+docker exec cli peer chaincode invoke -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"setSupply","Args":["G5H6J7K8","Arrived-S"]}'
+docker exec cli peer chaincode query -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"getSupply","Args":["G5H6J7K8"]}'
+
+docker exec cli peer chaincode invoke -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"setSupply","Args":["G5H6J7K8","true","user1"]}'
+docker exec cli peer chaincode query -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"getSupply","Args":["G5H6J7K8"]}'
 sleep 3
 # query chaincode for channelsales1
-docker exec cli peer chaincode query -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"getAllProduct","Args":["1Q2W3E4R"]}'
+docker exec cli peer chaincode invoke -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"getAllProduct","Args":["user1"]}'
+docker exec cli peer chaincode invoke -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"getAllProduct","Args":["user2"]}'
