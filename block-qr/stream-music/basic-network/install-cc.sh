19fc758fe1cf736a7a92e9446c3f9d7cbab54aa4
heeyeon9 <78019287+heeyeon9@users.noreply.github.com>
2021-08-02 14:05:57 +0900

    Update install-cc.sh

--- block-qr/stream-music/basic-network/install-cc.sh
+++ block-qr/stream-music/basic-network/install-cc.sh
@@ -5,7 +5,7 @@ set -ev
 docker exec cli peer chaincode install -n product-cc -v 1.0 -p chaincode/go
 sleep 1
 # instantiate chaincode for channelsales1
-docker exec cli peer chaincode instantiate -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -v 1.0 -c '{"Args":[""]}' -P "OR ('Sales1Org.member','CustomerOrg.member')"
+docker exec cli peer chaincode instantiate -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -v 1.0 -c '{"Args":[""]}' -P "OR ('Sales1Org.member')"
 sleep 10
 docker exec cli peer chaincode invoke -o orderer.acornpub.com:7050 -C channelsales1 -n product-cc -c '{"function":"initSupply","Args":[""]}'
 sleep 3
