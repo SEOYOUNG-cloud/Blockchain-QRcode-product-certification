8d9c726ede391cbdcbff0a773bdb51e54a3411e1
heeyeon9 <78019287+heeyeon9@users.noreply.github.com>
2021-08-12 18:24:59 +0900

    Update start-ca.sh

--- block-qr/stream-music/basic-network/start-ca.sh
+++ block-qr/stream-music/basic-network/start-ca.sh
@@ -9,12 +9,3 @@ cd application/sdk
 node enrollAdmin.js
 sleep 1
 node registUsers.js
-sleep 1
-node setProduct.js Z1X2C3V4 602204 Gucci
-node setSupply.js Z1X2C3V4 Arrived-D none
-node setSupply.js Z1X2C3V4 Arrived-S none
-node setSupply.js Z1X2C3V4 true user2
-sleep 1
-node getSupply.js Z1X2C3V4
-sleep 1
-node getAllProduct.js user2
