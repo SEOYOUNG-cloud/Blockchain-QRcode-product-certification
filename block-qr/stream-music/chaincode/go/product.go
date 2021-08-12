37880053d3062630e2aac0175a5e96b889a3bd75
heeyeon9 <78019287+heeyeon9@users.noreply.github.com>
2021-08-12 18:21:45 +0900

    Update product.go

--- block-qr/stream-music/chaincode/go/product.go
+++ block-qr/stream-music/chaincode/go/product.go
@@ -30,6 +30,17 @@ type Product struct {
 	//WalletID    string `json:"walletid"`
 }
 
+type All struct {
+	Name string `json:"name"`
+	Brand string `json:"brand"`
+	Factory string `json:"factory"`
+	Delivery   string `json:"delivery"`
+	Store string `json:"store"`
+	Status bool `json:"status"`
+	UserID string `json:"userid"`
+	ProductID string `json:"productid"`
+}
+
 type SupplyKey struct {
 	Key string
 	Idx int
@@ -48,10 +59,12 @@ func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) pb.Response 
 		return s.getSupply(APIstub, args)
 	} else if function == "setProduct" {
 		return s.setProduct(APIstub, args)
-	} else if function == "getAllProduct" {
-		return s.getAllProduct(APIstub, args)
+	} else if function == "getProduct" {
+		return s.getProduct(APIstub, args)
 	} else if function == "setSupply" {
 		return s.setSupply(APIstub, args)
+	} else if function == "getAllProduct" {
+		return s.getAllProduct(APIstub)
 	}
 	fmt.Println("Please check your function : " + function)
 	return shim.Error("Unknown function")
@@ -59,9 +72,9 @@ func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) pb.Response 
 
 func (s *SmartContract) initSupply(APIstub shim.ChaincodeStubInterface) pb.Response {
 	//product
-	product1 := Product{SerialNum: "1Q2W3E4R", Name: "466492", Brand: "Gucci"}
-	product2 := Product{SerialNum: "5T6Y7U8I", Name: "456126", Brand: "Gucci"}
-	product3 := Product{SerialNum: "A1S2D3F4", Name: "AP0214", Brand: "Chanel"}
+	product1 := Product{SerialNum: "210812QWER1234", Name: "466492", Brand: "Gucci"}
+	product2 := Product{SerialNum: "210812TYUI5678", Name: "456126", Brand: "Gucci"}
+	product3 := Product{SerialNum: "210812ASDF4321", Name: "AP0214", Brand: "Chanel"}
 
 	product1AsJSONBytes, _ := json.Marshal(product1)
 	APIstub.PutState(product1.SerialNum + "pr", product1AsJSONBytes)
@@ -76,31 +89,75 @@ func (s *SmartContract) initSupply(APIstub shim.ChaincodeStubInterface) pb.Respo
 	var supplykey = SupplyKey{}
 	json.Unmarshal(generateKey(APIstub, "latestKey"), &supplykey)
 
-	supply1 := Supply{Factory: "Arrived-F", Delivery: "Arrived-D", Store: "none", Status: false, UserID: "none", ProductID: "1Q2W3E4R"}
-	supply2 := Supply{Factory: "Arrived-F", Delivery: "Arrived-D", Store: "Arrived-S", Status: false, UserID: "none", ProductID: "5T6Y7U8I"}
-	supply3 := Supply{Factory: "Arrived-F", Delivery: "Arrived-D", Store: "Arrived-S", Status: true, UserID: "user1", ProductID: "A1S2D3F4"}
+	supply1 := Supply{Factory: "Arrived-F", Delivery: "Arrived-D", Store: "none", Status: false, UserID: "none", ProductID: "210812QWER1234"}
+	supply2 := Supply{Factory: "Arrived-F", Delivery: "Arrived-D", Store: "Arrived-S", Status: false, UserID: "none", ProductID: "210812TYUI5678"}
+	supply3 := Supply{Factory: "Arrived-F", Delivery: "Arrived-D", Store: "Arrived-S", Status: true, UserID: "user2", ProductID: "210812ASDF4321"}
 
 	keyidx1 := strconv.Itoa(supplykey.Idx)
 	supply1AsJSONBytes, _ := json.Marshal(supply1)
 	var keyString = supplykey.Key + keyidx1
 	APIstub.PutState(keyString, supply1AsJSONBytes)
 	APIstub.PutState(supply1.ProductID + "su", supply1AsJSONBytes)
+	supplykey1AsBytes, _ := json.Marshal(supplykey)
+	APIstub.PutState(supply1.ProductID+"Key", supplykey1AsBytes)
 
 	keyidx2 := strconv.Itoa(supplykey.Idx + 1)
 	supply2AsJSONBytes, _ := json.Marshal(supply2)
 	keyString = supplykey.Key + keyidx2
 	APIstub.PutState(keyString, supply2AsJSONBytes)
 	APIstub.PutState(supply2.ProductID + "su", supply2AsJSONBytes)
+	supplykey.Idx = supplykey.Idx + 1
+	supplykey2AsBytes, _ := json.Marshal(supplykey)
+	APIstub.PutState(supply2.ProductID+"Key", supplykey2AsBytes)
 
-	keyidx3 := strconv.Itoa(supplykey.Idx + 2)
+	keyidx3 := strconv.Itoa(supplykey.Idx + 1)
 	supply3AsJSONBytes, _ := json.Marshal(supply3)
 	keyString = supplykey.Key + keyidx3
 	APIstub.PutState(keyString, supply3AsJSONBytes)
 	APIstub.PutState(supply3.ProductID + "su", supply3AsJSONBytes)
+	supplykey.Idx = supplykey.Idx + 1
+	supplykey3AsBytes, _ := json.Marshal(supplykey)
+	APIstub.PutState(supply3.ProductID+"Key", supplykey3AsBytes)
+	APIstub.PutState("latestKey", supplykey3AsBytes)
 
-	supplykey.Idx = supplykey.Idx + 2
-	supplykeyAsBytes, _ := json.Marshal(supplykey)
-	APIstub.PutState("latestKey", supplykeyAsBytes)
+	//all
+	var supplykey_all = SupplyKey{}
+	json.Unmarshal(generateKey_all(APIstub, "latestKey_all"), &supplykey_all)
+
+	all1 := All{Name: "466492", Brand: "Gucci", Factory: "Arrived-F", Delivery: "Arrived-D", Store: "none", Status: false, UserID: "none", ProductID: "210812QWER1234"}
+	all2 := All{Name: "456126", Brand: "Gucci", Factory: "Arrived-F", Delivery: "Arrived-D", Store: "Arrived-S", Status: false, UserID: "none", ProductID: "210812TYUI5678"}
+	all3 := All{Name: "AP0214", Brand: "Chanel", Factory: "Arrived-F", Delivery: "Arrived-D", Store: "Arrived-S", Status: true, UserID: "user2", ProductID: "210812ASDF4321"}
+	
+	keyidx1_all := strconv.Itoa(supplykey_all.Idx)
+	all1AsJSONBytes, _ := json.Marshal(all1)
+	var keyString_all = supplykey_all.Key + keyidx1_all
+	APIstub.PutState(keyString_all, all1AsJSONBytes)
+	APIstub.PutState(all1.ProductID + "all", all1AsJSONBytes)
+	supplykey1_allAsBytes, _ := json.Marshal(supplykey_all)
+	APIstub.PutState(all1.ProductID+"Key_all", supplykey1_allAsBytes)
+	var buffer bytes.Buffer
+	buffer.WriteString("keystring:"+ keyString_all)
+
+	keyidx2_all := strconv.Itoa(supplykey_all.Idx + 1)
+	all2AsJSONBytes, _ := json.Marshal(all2)
+	keyString_all = supplykey_all.Key + keyidx2_all
+	APIstub.PutState(keyString_all, all2AsJSONBytes)
+	APIstub.PutState(all2.ProductID + "all", all2AsJSONBytes)
+	supplykey_all.Idx = supplykey_all.Idx + 1
+	supplykey2_allAsBytes, _ := json.Marshal(supplykey_all)
+	APIstub.PutState(all2.ProductID+"Key_all", supplykey2_allAsBytes)
+	buffer.WriteString("keystring:"+ keyString_all)
+
+	keyidx3_all := strconv.Itoa(supplykey_all.Idx + 1)
+	all3AsJSONBytes, _ := json.Marshal(all3)
+	keyString_all = supplykey_all.Key + keyidx3_all
+	APIstub.PutState(keyString_all, all3AsJSONBytes)
+	APIstub.PutState(all3.ProductID + "all", all3AsJSONBytes)
+	supplykey_all.Idx = supplykey_all.Idx + 1
+	supplykey3_allAsBytes, _ := json.Marshal(supplykey_all)
+	APIstub.PutState(all3.ProductID+"Key_all", supplykey3_allAsBytes)
+	APIstub.PutState("latestKey_all", supplykey3_allAsBytes)
+	buffer.WriteString("keystring:"+ keyString_all)
 
 	return shim.Success(nil)
 }
@@ -115,8 +172,6 @@ func (s *SmartContract) getSupply(APIstub shim.ChaincodeStubInterface, args []st
 	}
 	
 	if err != nil {
-		//buffer.WriteString(args[0] + ": False")
-		// return shim.Error(args[0] + ": False")
 		fmt.Println(err.Error())
 	}
 
@@ -125,11 +180,7 @@ func (s *SmartContract) getSupply(APIstub shim.ChaincodeStubInterface, args []st
 
 	
 	buffer.WriteString("[")
-	// bArrayMemberAlreadyWritten := false
 
-	// if bArrayMemberAlreadyWritten == true {
-	// 	buffer.WriteString(",")
-	// }
 	buffer.WriteString("{\"SerialNum\":")
 	buffer.WriteString("\"")
 	buffer.WriteString(product.SerialNum)
@@ -146,8 +197,7 @@ func (s *SmartContract) getSupply(APIstub shim.ChaincodeStubInterface, args []st
 	buffer.WriteString("\"")
 
 	buffer.WriteString("}")
-	// bArrayMemberAlreadyWritten = true
-	buffer.WriteString("]\n")
+	buffer.WriteString("]")
 
 	supply := Supply{}
 	supplyAsBytes, err := APIstub.GetState(args[0] + "su")
@@ -180,7 +230,7 @@ func (s *SmartContract) getSupply(APIstub shim.ChaincodeStubInterface, args []st
 	buffer.WriteString("\"")
 
 	buffer.WriteString("}")
-	buffer.WriteString("]\n")
+	buffer.WriteString("]")
 
 	if supply.Store == "none" && supply.Status == false {
 		buffer.WriteString(args[0] + ": True, not on sale.")
@@ -205,10 +255,6 @@ func generateKey(APIstub shim.ChaincodeStubInterface, key string) []byte {
 
 	supplykey := SupplyKey{}
 	json.Unmarshal(productkeyAsBytes, &supplykey)
-	// var tempIdx string
-	// tempIdx = strconv.Itoa(musickey.Idx)
-	// fmt.Println(musickey)
-	// fmt.Println("Key is " + strconv.Itoa(len(musickey.Key)))
 	if len(supplykey.Key) == 0 || supplykey.Key == "" {
 		isFirst = true
 		supplykey.Key = "SU"
@@ -217,7 +263,30 @@ func generateKey(APIstub shim.ChaincodeStubInterface, key string) []byte {
 		supplykey.Idx = supplykey.Idx + 1
 	}
 
-	// fmt.Println("Last MusicKey is " + musickey.Key + " : " + tempIdx)
+	returnValueBytes, _ := json.Marshal(supplykey)
+
+	return returnValueBytes
+}
+
+func generateKey_all(APIstub shim.ChaincodeStubInterface, key string) []byte {
+
+	var isFirst bool = false
+
+	productkeyAsBytes, err := APIstub.GetState(key)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	supplykey := SupplyKey{}
+	json.Unmarshal(productkeyAsBytes, &supplykey)
+	if len(supplykey.Key) == 0 || supplykey.Key == "" {
+		isFirst = true
+		supplykey.Key = "ALL"
+	}
+	if !isFirst {
+		supplykey.Idx = supplykey.Idx + 1
+	}
+
 	returnValueBytes, _ := json.Marshal(supplykey)
 
 	return returnValueBytes
@@ -252,12 +321,28 @@ func (s *SmartContract) setProduct(APIstub shim.ChaincodeStubInterface, args []s
 	APIstub.PutState("latestKey", supplykeyAsBytes)
 	APIstub.PutState(args[0]+"Key", supplykeyAsBytes)
 
-	// return shim.Success(nil)
+	//all
+	var supplykey_all = SupplyKey{}
+	json.Unmarshal(generateKey_all(APIstub, "latestKey_all"), &supplykey_all)
+	keyidx_all := strconv.Itoa(supplykey_all.Idx)
+
+	var all = All{Name: args[1], Brand: args[2], Factory: "Arrived-F", Delivery: "none", Store: "none", Status: false, UserID: "none", ProductID: args[0]}
+
+	allAsJSONBytes, _ := json.Marshal(all)
+	var keyString_all = supplykey_all.Key + keyidx_all
+	APIstub.PutState(keyString_all, allAsJSONBytes)
+	APIstub.PutState(all.ProductID + "all", allAsJSONBytes)
+	buffer.WriteString("keystring:"+ keyString_all)
+
+	supplykey_allAsBytes, _ := json.Marshal(supplykey_all)
+	APIstub.PutState("latestKey_all", supplykey_allAsBytes)
+	APIstub.PutState(args[0]+"Key_all", supplykey_allAsBytes)
+
 	return shim.Success(buffer.Bytes())
 	
 }
 
-func (s *SmartContract) getAllProduct(APIstub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (s *SmartContract) getProduct(APIstub shim.ChaincodeStubInterface, args []string) pb.Response {
 	
 	// Find latestKey
 	supplykeyAsBytes, _ := APIstub.GetState("latestKey")
@@ -279,7 +364,7 @@ func (s *SmartContract) getAllProduct(APIstub shim.ChaincodeStubInterface, args 
 	var buffer bytes.Buffer
 	supply := Supply{}
 	product := Product{}
-	// var isTrue bool = false
+
 	buffer.WriteString("\n[")
 	bArrayMemberAlreadyWritten := false
 	for resultsIter.HasNext() {
@@ -293,13 +378,9 @@ func (s *SmartContract) getAllProduct(APIstub shim.ChaincodeStubInterface, args 
 		json.Unmarshal(productAsBytes, &product)
 		if args[0] != supply.UserID {
 			continue
-			// isTrue = true
+
 		}
 		buffer.WriteString(supply.UserID)
-
-		// if err != nil {
-		// 	fmt.Println(err.Error())
-		// }
 		
 		if bArrayMemberAlreadyWritten == true {
 			buffer.WriteString(",")
@@ -320,12 +401,11 @@ func (s *SmartContract) getAllProduct(APIstub shim.ChaincodeStubInterface, args 
 		buffer.WriteString(product.Brand)
 		buffer.WriteString("\"")
 		
-		//buffer.WriteString(string(queryResponse.Value))
 		buffer.WriteString("}")
 		bArrayMemberAlreadyWritten = true
 		
 	}
-	buffer.WriteString("]\n")
+	buffer.WriteString("]")
 	
 	if bArrayMemberAlreadyWritten == false {
 		buffer.WriteString("no list for " + args[0])
@@ -335,32 +415,8 @@ func (s *SmartContract) getAllProduct(APIstub shim.ChaincodeStubInterface, args 
 }
 
 func (s *SmartContract) setSupply(APIstub shim.ChaincodeStubInterface, args []string) pb.Response {
-	// var tokenFromKey, tokenToKey int // Asset holdings
-	// var musicprice int // Transaction value
-	// var err error
 	var b bool
 
-	// if len(args) != 2 {
-	// 	return shim.Error("Incorrect number of arguments. Expecting 2")
-	// }
-
-	// musicAsBytes, err := APIstub.GetState(args[1])
-	// if err != nil {
-	// 	return shim.Error(err.Error())
-	// }
-	// music := Music{}
-	// json.Unmarshal(musicAsBytes, &music)
-	// musicprice, _ = strconv.Atoi(music.Price)
-	// SellerAsBytes, err := APIstub.GetState(music.WalletID)
-	// if err != nil {
-	// 	return shim.Error("Failed to get state")
-	// }
-	// if SellerAsBytes == nil {
-	// 	return shim.Error("Entity not found")
-	// }
-	// seller := Wallet{}
-	// json.Unmarshal(SellerAsBytes, &seller)
-	// tokenToKey, _ = strconv.Atoi(seller.Token)
 	SupplyAsBytes, err := APIstub.GetState(args[0] + "su")
 	if err != nil {
 		return shim.Error("Failed to get state")
@@ -370,47 +426,60 @@ func (s *SmartContract) setSupply(APIstub shim.ChaincodeStubInterface, args []st
 	}
 	supply := Supply{}
 	json.Unmarshal(SupplyAsBytes, &supply)
-	// tokenFromKey, _ = strconv.Atoi(customer.Token)
-	// customer.Token = strconv.Itoa(tokenFromKey - musicprice)
-	// seller.Token = strconv.Itoa(tokenToKey + musicprice)
+
+	//all
+	allAsBytes, err := APIstub.GetState(args[0] + "all")
+	if err != nil {
+		return shim.Error("Failed to get state")
+	}
+	if SupplyAsBytes == nil {
+		return shim.Error("Entity not found")
+	}
+	all := All{}
+	json.Unmarshal(allAsBytes, &all)
 
 	if args[1] == "Arrived-D" {
 		supply.Delivery = args[1]
+		all.Delivery = args[1]
 	} else if args[1] == "Arrived-S" {
 		supply.Store = args[1]
+		all.Store = args[1]
 	} else if args[1] == "true" {
 		b, _ = strconv.ParseBool(args[1])
 		supply.Status = b
 		supply.UserID = args[2]
+		all.Status = b
+		all.UserID = args[2]
 	}
 
 	supplykeyAsBytes, _ := APIstub.GetState(supply.ProductID+"Key")
 	supplykey := SupplyKey{}
 	json.Unmarshal(supplykeyAsBytes, &supplykey)
-	// var supplykey = SupplyKey{}
-	// json.Unmarshal(generateKey(APIstub, "latestKey"), &supplykey)
-	// keyidx := strconv.Itoa(supplykey.Idx)
-	// var keyString = supplykey.Key + keyidx
-	
 	
 	updatedSupplyAsBytes, _ := json.Marshal(supply)
 
-	
-	//updatedSellerAsBytes, _ := json.Marshal(seller)
 	APIstub.PutState(supply.ProductID + "su", updatedSupplyAsBytes)
 
 	keyidx := strconv.Itoa(supplykey.Idx)
 	var keyString = supplykey.Key + keyidx
 	APIstub.PutState(keyString, updatedSupplyAsBytes)
 
-	// productAsBytes, err := APIstub.GetState(supply.ProductID + "pr") //update product
-	// APIstub.PutState(supply.ProductID + "pr", productAsBytes)
+	//all
+	supplykey_allAsBytes, _ := APIstub.GetState(all.ProductID+"Key_all")
+	supplykey_all := SupplyKey{}
+	json.Unmarshal(supplykey_allAsBytes, &supplykey_all)
+	
+	updatedAllAsBytes, _ := json.Marshal(all)
 
+	APIstub.PutState(all.ProductID + "all", updatedAllAsBytes)
 
-	//APIstub.PutState(music.WalletID, updatedSellerAsBytes)
-	// buffer is a JSON array containing QueryResults
+	keyidx_all := strconv.Itoa(supplykey_all.Idx)
+	var keyString_all = supplykey_all.Key + keyidx_all
+	APIstub.PutState(keyString_all, updatedAllAsBytes)
+	
+
 	var buffer bytes.Buffer
-	buffer.WriteString("keystring:"+ keyString)
+	buffer.WriteString("keystring:"+ keyString_all)
 	buffer.WriteString("[")
 	buffer.WriteString("{\"Updated Factory\":")
 	buffer.WriteString("\"")
@@ -437,8 +506,50 @@ func (s *SmartContract) setSupply(APIstub shim.ChaincodeStubInterface, args []st
 	buffer.WriteString(supply.UserID)
 	buffer.WriteString("\"")
 	buffer.WriteString("}")
-	buffer.WriteString("]\n")
+	buffer.WriteString("]")
 
+	return shim.Success(buffer.Bytes())
+}
+func (s *SmartContract) getAllProduct(APIstub shim.ChaincodeStubInterface) pb.Response {
+	
+	supplykey_allAsBytes, _ := APIstub.GetState("latestKey_all")
+	supplykey_all := SupplyKey{}
+	json.Unmarshal(supplykey_allAsBytes, &supplykey_all)
+	idxStr := strconv.Itoa(supplykey_all.Idx + 1)
+
+	var startKey = "ALL0"
+	var endKey = supplykey_all.Key + idxStr
+
+	resultsIter, err := APIstub.GetStateByRange(startKey, endKey)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer resultsIter.Close()
+	
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+	bArrayMemberAlreadyWritten := false
+	for resultsIter.HasNext() {
+		queryResponse, err := resultsIter.Next()
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Record\":")
+		
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
 	return shim.Success(buffer.Bytes())
 }
 
