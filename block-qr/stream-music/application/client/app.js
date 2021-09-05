17c785b9cb57762db3dc6f49f4a74128c7f836b1
heeyeon9 <78019287+heeyeon9@users.noreply.github.com>
2021-09-06 02:54:16 +0900

    Update app.js

--- block-qr/stream-music/application/client/app.js
+++ block-qr/stream-music/application/client/app.js
@@ -1,23 +1,20 @@
 'use strict';
 var app = angular.module('application', []);
 app.controller('AppCtrl', function($scope, appFactory){
-        $("#success_setproduct").hide();
-        $("#success_getallproduct").hide();
-       $scope.getAllProduct = function(){
+        $(document).ready(function(){
                 appFactory.getAllProduct(function(data){
                         var array = [];
                         for (var i = 0; i < data.length; i++){
                                 parseInt(data[i].Key);
                                 data[i].Record.Key = data[i].Key;
                                 array.push(data[i].Record);
-                                $("#success_getallproduct").hide();
                         }
-                        array.sort(function(a, b) {
-                            return parseFloat(a.Key) - parseFloat(b.Key);
-                        });
-                        $scope.allProduct = array;
+                array.sort(function(a, b) {
+                        return parseFloat(a.Key) - parseFloat(b.Key);
                 });
-        }
+                $scope.allProduct = array;
+                });
+        });
         $scope.setProduct = function(){
             appFactory.setProduct($scope.product, function(data){
                         $scope.create_product = data;
