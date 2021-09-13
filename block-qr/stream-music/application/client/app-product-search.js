3bab41749ae95abcea801e6f48c1479c86ad073d
heeyeon9 <78019287+heeyeon9@users.noreply.github.com>
2021-09-13 14:59:12 +0900

    Update app-product-search.js

--- block-qr/stream-music/application/client/app-product-search.js
+++ block-qr/stream-music/application/client/app-product-search.js
@@ -1,30 +1,37 @@
 'use strict';
 var app = angular.module('application', []);
 app.controller('AppCtrl', function($scope, appFactory){
-        $scope.getSerial = function(){
-          var option = document.getElementsByClassName("label");
-          $scope.search.option = option[0].innerHTML;
 
-                appFactory.getSerial($scope.search, function(data){
-                        var array = [];
-                        for (var i = 0; i < data.length; i++){
-                                parseInt(data[i].Key);
-                                data[i].Record.Key = data[i].Key;
-                                array.push(data[i].Record);
-                        }
+    var getKeyboardEventResult = function (keyEvent) {
+      return (window.event ? keyEvent.keyCode : keyEvent.which);
+    };
+
+    $scope.getSerial = function($event){
+        if(getKeyboardEventResult($event) == 13) {
+            var option = document.getElementsByClassName("label");
+            $scope.search.option = option[0].innerHTML;
+
+            appFactory.getSerial($scope.search, function(data){
+                var array = [];
+                for (var i = 0; i < data.length; i++){
+                    parseInt(data[i].Key);
+                    data[i].Record.Key = data[i].Key;
+                    array.push(data[i].Record);
+                }
                 array.sort(function(a, b) {
-                        return parseFloat(a.Key) - parseFloat(b.Key);
+                    return parseFloat(a.Key) - parseFloat(b.Key);
                 });
                 $scope.allProduct = array;
-                });
+            });
         }
+    }
 });
  app.factory('appFactory', function($http){
-        var factory = {};
-        factory.getSerial = function(data, callback){
-            $http.get('/api/getSerial?option='+data.option+'&searchWord='+data.searchWord).success(function(output){
-                        callback(output)
-                });
-        }
-        return factory;
+    var factory = {};
+    factory.getSerial = function(data, callback){
+        $http.get('/api/getSerial?option='+data.option+'&searchWord='+data.searchWord).success(function(output){
+            callback(output)
+        });
+    }
+    return factory;
 });
