a87c5744fca9cd001d1debfd95bfcd41a6fc0de5
SEOYOUNG-cloud <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2021-09-28 21:41:34 +0900

    add complete toolbar
    add complete screen toolbar

--- PARM/app/src/main/java/com/example/PARM/dis_registerQR_OK.java
+++ PARM/app/src/main/java/com/example/PARM/dis_registerQR_OK.java
@@ -1,10 +1,14 @@
 package com.example.PARM;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 
 public class dis_registerQR_OK extends AppCompatActivity {
 
@@ -15,6 +19,13 @@ public class dis_registerQR_OK extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dis_save);
 
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false); // 기본 제목을 없애기
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼
+
 
         Button disHomeButton = (Button) findViewById(R.id.disHomeButton);
 
@@ -24,5 +35,16 @@ public class dis_registerQR_OK extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: { // 왼쪽 상단 버튼 눌렀을 때
+                finish();
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- PARM/app/src/main/java/com/example/PARM/manu_registerQR_OK.java
+++ PARM/app/src/main/java/com/example/PARM/manu_registerQR_OK.java
@@ -2,10 +2,13 @@ package com.example.PARM;
 
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -18,6 +21,13 @@ public class manu_registerQR_OK extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manu_save);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false); // 기본 제목을 없애기
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼
 
 
         Button facHomeButton = (Button) findViewById(R.id.facHomeButton);
@@ -34,5 +44,16 @@ public class manu_registerQR_OK extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: { // 왼쪽 상단 버튼 눌렀을 때
+                finish();
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
