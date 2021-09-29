619813b00228e1d54b84da39fa379d2652ca7ac1
SEOYOUNG-cloud <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2021-09-29 18:22:56 +0900

    modify toolbar
    modify toolbar

--- PARM/app/src/main/java/com/example/PARM/shop_sale_OK.java
+++ PARM/app/src/main/java/com/example/PARM/shop_sale_OK.java
@@ -1,10 +1,13 @@
 package com.example.PARM;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class shop_sale_OK extends AppCompatActivity {
 
@@ -13,6 +16,14 @@ public class shop_sale_OK extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shop_sale_complete);
+
+        //툴바
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false); // 기본 제목을 없애기
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼
 
         Button shophomebtn = (Button) findViewById(R.id.saleHomeButton);
 
@@ -23,6 +34,16 @@ public class shop_sale_OK extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: { // 왼쪽 상단 버튼 눌렀을 때
+                finish();
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
