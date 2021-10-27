f88f23b2fe0dd8b5e538ff564103a814e0da7319
heeyeon9 <78019287+heeyeon9@users.noreply.github.com>
2021-10-27 22:40:37 +0900

    Update server.js

--- block-qr/stream-music/application/server/server.js
+++ block-qr/stream-music/application/server/server.js
@@ -61,7 +61,7 @@ app.get('/index',function(req,res){
   if (req.session.user) {
     res.render('index', {user :req.session.user});
   } else {
-    res.render('index', {user :''});
+    res.redirect('/');
   }
 });
 
